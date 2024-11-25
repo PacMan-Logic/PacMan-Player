81062cd83ab15f4ffb5b77a1e351dbeed2e743a0
Rosist <umaumaliu@outlook.com>
2024-11-25 10:18:06 +0800

    Add Screen Check

--- Assets/Scripts/Communication/Interaction.cs
+++ Assets/Scripts/Communication/Interaction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Enums;
 using Unity.VisualScripting;
+using UnityEngine.EventSystems;
 
 public class Interaction : MonoBehaviour
 {
@@ -11,25 +12,28 @@ public class Interaction : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetMouseButtonDown(0))
         {
-            action = MovementType.Up;
-            Debug.Log("Up");
-        }
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            action = MovementType.Down;
-            Debug.Log("Down");
-        }
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            action = MovementType.Right;
-            Debug.Log("Right");
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            action = MovementType.Left;
-            Debug.Log("Left");
+            if (IsPointerOverUI())
+            {
+                return;
+            }
+            float screenHeight = Screen.height / 4;
+            float screenWidth = Screen.width / 2;
+            Vector3 MousePosition = Input.mousePosition;
+            if(MousePosition.y > 3 * screenHeight)
+            {
+                Debug.Log("up");
+            }else if(MousePosition.y < screenHeight)
+            {
+                Debug.Log("down");
+            }else if(MousePosition.x < screenWidth)
+            {
+                Debug.Log("left");
+            }else if(MousePosition.x >  screenWidth)
+            {
+                Debug.Log("right");
+            }
         }
     }
 
@@ -41,5 +45,10 @@ public class Interaction : MonoBehaviour
             Debug.Log("No Message");
         }
         //SendToFrontend(msg);
+    }
+
+    private bool IsPointerOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
     }
 }
