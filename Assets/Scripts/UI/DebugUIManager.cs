236455d8958da94fdf368fce81fa4824f95550dd
Rosist <umaumaliu@outlook.com>
2024-12-16 16:23:52 +0800

    feat:switch debug mode

---
+++ Assets/Scripts/UI/DebugUIManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas canvas; // 需要控制的 Canvas
+    public Button toggleButton; // 触发控制的按钮
+
+    private bool isCanvasVisible = false; // Canvas 是否可见的状态
+
+    void Start()
+    {
+        // 为按钮添加点击事件
+        toggleButton.onClick.AddListener(ToggleCanvasVisibility);
+        canvas.gameObject.SetActive(isCanvasVisible); // 显示或隐藏 Canvas
+        Debug.Log("Init Success");
+    }
+    
+    void ToggleCanvasVisibility()
+    {
+        isCanvasVisible = !isCanvasVisible;
+        canvas.gameObject.SetActive(isCanvasVisible); // 显示或隐藏 Canvas
+        Debug.Log("Changed");
+    }
+}
